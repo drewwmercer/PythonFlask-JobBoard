baed0c31639154d85904ec2df2d64d3ad9b6ed79
Drew Mercer <japanexpress@gmail.com>
2023-01-03 11:26:57 -0500

    Working on the sqlite connection

--- jobs/app.py
+++ jobs/app.py
@@ -20,27 +20,27 @@ def open_connection():
     return connection
 
 
-def execute_sql(sql, values, commit, single):
+def execute_sql(sql, values=(), commit=False, single=False):
     connection = open_connection()
-    
-    values = ()
-    commit = False
-    single = False
-    
+
     cursor = connection.execute(sql, values)
-    
+
     if commit == True:
         results = connection.commit()
     else:
-        
+        results = cursor.fetchone() if single else cursor.fetchall()
+
+    cursor.close()
+
     return results
 
+
+@app.teardown_appcontext
 def close_connection(exception):
     connection = getattr(g, '_connection', None)
-    
+
     if connection != None:
         connection.close()
-        @app.teardown_appcontext
 
 
 @app.route('/')
